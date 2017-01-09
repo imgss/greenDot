58629f563077b631858c7f35cfad8b611a7b47ed
imgss <838632526@qq.com>
2017-01-09 23:38:57 +0800

    bug:传入chart.js 的数据无法绘制成图像

--- express.js
+++ express.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 app.set('views', './views');
 app.set('view engine', 'jade');
-module.exports = function(data) {
+module.exports = function(wdata, mdata) {
     app.get('/', function(req, res) {
-        res.render('chart.jade', { dayData: data });
+        res.render('chart.jade', { weekData: wdata, monthData: mdata });
     });
     app.listen(3000);
 }
--- getData.js
+++ getData.js
@@ -21,24 +21,37 @@ function getData(url) {
             $ = cheerio.load(pageStr);
             var days = $("g rect");
             for (var i = 0; i < days.length; i++) {
-                dayDatas.push(parseInt($(days[i]).attr('data-count')));
+                var daycount = parseInt($(days[i]).attr('data-count')); //日贡献
+                var date = $(days[i]).attr('data-date'); //日期
+                var newObj = {};
+                newObj.date = date;
+                newObj.data = daycount;
+                dayDatas.push(newObj);
             }
-            dayDatas = _.chunk(dayDatas, 7);
-            emitter.emit('dataDone', dayDatas);
-            console.log(dayDatas);
+            weekDatas = _.chunk(dayDatas, 7);
+            console.log('weekDatas');
+            dayDatas = _.filter(dayDatas, function(o) { //过滤出2016年的数据
+                return (o.date.split('-'))[0] == '2016';
+            })
+            monthDatas = _.groupBy(dayDatas, function(o) {
+                return (o.date.split('-'))[1];
+            });
+            //console.log(JSON.stringify(monthDatas));
+            emitter.emit('dataDone', JSON.stringify(weekDatas), JSON.stringify(monthDatas));
+
 
         });
     });
 }
 getData(queryStr);
-emitter.on('dataDone', function(data) {
+emitter.on('dataDone', function(weekdata, monthdata) {
     // http.createServer(function(req, res) {
     //     res.setHeader('Content-Type', 'text/html');
     //     res.writeHead(200);
     //     res.end('data.toString()');
     //     console.log('done');
     // }).listen(3000, '127.0.0.1');
-    app(data);
+    app(weekdata, monthdata);
 
 })
 
